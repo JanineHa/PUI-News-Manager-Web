3b743dc366258f5b7d2c305009971d90e09e901a
Manon Epplée <manon.epplee@gmail.com>
2022-10-09 18:08:36 +0200

    add missing image attributes to Article interface

--- src/app/interfaces/article.ts
+++ src/app/interfaces/article.ts
@@ -5,4 +5,8 @@ export interface Article {
   category: string;
   abstract: string;
   body?: string;
+  image_media_type?: string;
+  image_data?: string;
+  thumbnail_image?: string;
+  thumbnail_media_type?: string;
 }
