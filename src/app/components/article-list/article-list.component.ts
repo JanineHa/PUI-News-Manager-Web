5c8fe9ce20fc9b76b7a9b0f8bd2b2e7d6545f9d8
Manon Epplée <manon.epplee@gmail.com>
2022-10-10 14:40:36 +0200

    add article list functionality and content

--- src/app/components/article-list/article-list.component.ts
+++ src/app/components/article-list/article-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from 'src/app/interfaces/article';
+import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-article-list',
@@ -7,7 +8,22 @@ import { Article } from 'src/app/interfaces/article';
   styleUrls: ['./article-list.component.scss'],
 })
 export class ArticleListComponent implements OnInit {
-  constructor() {}
+  articleList: Article[]
 
-  ngOnInit(): void {}
+  constructor(private newsService: NewsService) {
+    this.articleList = []
+    this.getArticleList()
+   }
+
+  ngOnInit(): void { }
+
+  getArticleList(): void {
+    this.newsService.getArticles().subscribe(list => this.articleList = list)
+  }
+
+  removeArticle(id: any):void {
+    if (window.confirm("Do you really want to delete this article?")) {
+      this.newsService.deleteArticle(Number(id)).subscribe()
+    }
+  }
 }
