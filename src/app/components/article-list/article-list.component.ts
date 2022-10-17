1aadc8276e1fb5877012f0f96e1a22d14bb5003b
Manon Epplée <manon.epplee@gmail.com>
2022-10-17 19:19:55 +0200

    rework article list

--- src/app/components/article-list/article-list.component.ts
+++ src/app/components/article-list/article-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FilterService } from 'src/app/services/filter.service';
+import { FlashService } from 'src/app/services/flash.service';
 import { Article } from 'src/app/interfaces/article';
+import { LoginService } from 'src/app/services/login.service';
 import { NewsService } from 'src/app/services/news.service';
 
 @Component({
@@ -9,21 +12,47 @@ import { NewsService } from 'src/app/services/news.service';
 })
 export class ArticleListComponent implements OnInit {
   articleList: Article[]
+  flashMessage: string | null
 
-  constructor(private newsService: NewsService) {
+  constructor(private newsService: NewsService, private loginService: LoginService,
+    private flashService: FlashService, private filterService: FilterService) {
     this.articleList = []
+    this.flashMessage = flashService.getFlashMessage()
+   }
+
+  ngOnInit(): void {
     this.getArticleList()
    }
 
-  ngOnInit(): void { }
-
   getArticleList(): void {
     this.newsService.getArticles().subscribe(list => this.articleList = list)
+
+  }
+
+  userLogged(): Boolean {
+    return this.loginService.isLogged()
   }
 
   removeArticle(id: any):void {
     if (window.confirm("Do you really want to delete this article?")) {
-      this.newsService.deleteArticle(Number(id)).subscribe()
+      this.newsService.deleteArticle(Number(id)).subscribe(x => {
+        this.newsService.getArticles().subscribe(list => this.articleList = list)
+        this.flashService.setFlashMessage("Article was removed successfully!")
+        this.flashMessage = this.flashService.getFlashMessage()
+      })
     }
+  }
+
+  hideFlash(): void {
+    this.flashService.removeFlashMessage()
+    this.flashMessage = this.flashService.getFlashMessage()
+  }
+
+  getFilterTerm(): string {
+    return this.filterService.getFilterTerm()
+  }
+
+  getCategory(): string {
+    return this.filterService.getCategory()
   }
 }
